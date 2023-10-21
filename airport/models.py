c6d1bbab310ddd668ce9069e185aa4a31b9fba93
Roman Podolets <romanpyu@gmail.com>
2023-10-21 23:42:06 +0300

    Add tickets and routes validators

--- airport/models.py
+++ airport/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.core.exceptions import ValidationError
 
 
 class Airport(models.Model):
@@ -59,6 +60,9 @@ class Route(models.Model):
             f"Distance: {self.distance}"
         )
 
+    class Meta:
+        unique_together = ("source", "destination")
+
 
 class Flight(models.Model):
     route = models.ForeignKey(
@@ -100,3 +104,40 @@ class Ticket(models.Model):
     class Meta:
         unique_together = ("flight", "row", "seat")
         ordering = ["row", "seat"]
+
+    @staticmethod
+    def validate_ticket(row, seat, flight, error_to_raise):
+        for ticket_attr_value, ticket_attr_name, flight_attr_name in [
+            (row, "row", "rows"),
+            (seat, "seat", "seats_in_row"),
+        ]:
+            count_attrs = getattr(flight, flight_attr_name)
+            if not (1 <= ticket_attr_value <= count_attrs):
+                raise error_to_raise(
+                    {
+                        ticket_attr_name: f"{ticket_attr_name} "
+                                          f"number must be in available range: "
+                                          f"(1, {flight_attr_name}): "
+                                          f"(1, {count_attrs})"
+                    }
+                )
+
+    def clean(self):
+        Ticket.validate_ticket(
+            self.row,
+            self.seat,
+            self.flight.airplane,
+            ValidationError,
+        )
+
+    def save(
+            self,
+            force_insert=False,
+            force_update=False,
+            using=None,
+            update_fields=None,
+    ):
+        self.full_clean()
+        return super(Ticket, self).save(
+            force_insert, force_update, using, update_fields
+        )
